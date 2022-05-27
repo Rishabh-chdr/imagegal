bd69de619e5d46642c122929f17004b11af6d25f
Rishabh Ch <17faltu@gmail.com>
2022-05-27 13:38:24 +0530

    frontend first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './Css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App">
+      <h1>Images Gallery</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
