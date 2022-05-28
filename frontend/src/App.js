d3dd0e76dab483ede622d688576941d10544c1a4
Rishabh Ch <17faltu@gmail.com>
2022-05-28 22:24:30 +0530

    eslint & prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,33 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import Header from './components/Header';
-import Search from './components/Search';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
 const key = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
-  const [word, setWord] = useState('');
-  
+  const [word, setWord] = useState("");
+
   const handlesearchsubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${key}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${key}`
+    )
       .then((res) => res.json())
-      .then((data) => {console.log(data);})
+      .then((data) => {
+        console.log(data);
+      })
       .catch((err) => {
         console.log(err);
-        })
-    setWord('');
-  }
- 
+      });
+    setWord("");
+  };
 
   return (
-    <div >
+    <div>
       <Header />
-      <Search word={word} setWord = {setWord} handlesubmit={handlesearchsubmit}/>
+      <Search word={word} setWord={setWord} handlesubmit={handlesearchsubmit} />
     </div>
   );
-}
+};
 
 export default App;
